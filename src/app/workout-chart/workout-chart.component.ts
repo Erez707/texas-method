d8c3fcdd59529b5e703e2f311cc5808c13c4e51c
Erez707 <47011834+Erez707@users.noreply.github.com>
2022-07-03 22:12:16 -0400

    add chart with dummy data, needs more work

--- src/app/workout-chart/workout-chart.component.ts
+++ src/app/workout-chart/workout-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChartData } from 'chart.js';
 
 @Component({
   selector: 'workout-chart',
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workout-chart.component.scss']
 })
 export class WorkoutChartComponent implements OnInit {
+  @Input() workoutType!: string;
 
-  constructor() { }
+  volumeData: ChartData;
+
+  constructor() { 
+    this.volumeData = {
+      labels: ['day0', 'day1', 'day2', 'day3'], // x-axis 
+      datasets: [{
+        label: this.workoutType,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 135, 145, 185], // y-axis
+      }]
+    }
+   }
 
   ngOnInit(): void {
   }
